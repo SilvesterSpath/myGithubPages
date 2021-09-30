6f96cbdde52283b02fce6269b4bdb2c815bfa758
SilvesterSpath <spath.sz@gmail.com>
2021-09-30 08:05:52 +0200

    be afraid...

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const scroll = document.querySelector('.scroll');
 console.log(container);
 
 const links = [
+  'https://silvesterspath.github.io/Countdown/',
   'https://silvesterspath.github.io/Full-stack-MERN/',
   'https://silvesterspath.github.io/Full-stack-MEAN/',
   'https://silvesterspath.github.io/Web-Speech/',
@@ -38,6 +39,7 @@ const links = [
 ];
 
 const images = [
+  './img/halloween.png',
   './img/full-stack.png',
   './img/mean_stack.png',
   './img/webspeech.png',
@@ -72,6 +74,7 @@ const images = [
 ];
 
 const texts = [
+  'Be afraid...',
   'Full-Stack MERN',
   'Full-Stack MEAN',
   'Say My Words',
